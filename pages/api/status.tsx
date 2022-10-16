b1d913006bfd6c098dfad001fdce7b2d4f01f22e
Aditya Agarwal <adityag2511@gmail.com>
2022-10-16 14:13:39 -0500

    Added API calls to fetch graph status

--- pages/api/status.tsx
+++ pages/api/status.tsx
@@ -57,3 +57,91 @@ const QUERY_BODY = `{
   }
   node
 }`
+
+export interface SubgraphIndexingStatus {
+  subgraph: string
+  synced: boolean
+  health: 'healthy' | 'unhealthy' | 'failed'
+  fatalError?: SubgraphError
+  // it may or may not be in the response body
+  nonFatalErrors?: SubgraphError[]
+  chains: ChainIndexingStatus[]
+  entityCount: string
+  node?: string
+}
+
+axios.interceptors.request.use((request) => {
+  console.log('Request:', JSON.stringify(request, null, 2))
+  return request
+})
+axios.interceptors.response.use(response => {
+  console.log('Response:', JSON.stringify(response, null, 2))
+  return response
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Result<Array<SubgraphIndexingStatus>>>
+) {
+  const subgraphID = req.query['subgraphID'] as string
+  try {
+    const data = await fetchStatus(subgraphID)
+    if (data === null) {
+      res.status(200).send({
+        error: {
+          message: 'subgraph not found',
+        },
+      })
+      return
+    }
+    res.status(200).send({
+      data,
+    })
+  } catch (error: any) {
+    console.log(JSON.stringify(error, Object.getOwnPropertyNames(error)))
+    res.status(200).send({
+      error: {
+        message: 'unknown error',
+      },
+    })
+  }
+}
+
+async function fetchStatus(
+  subgraphID: string
+): Promise<Array<SubgraphIndexingStatus> | null> {
+  if (isValidID(subgraphID)) {
+    const query = `{ indexingStatuses(subgraphs:["${subgraphID}"])${QUERY_BODY} }`
+    const data = (await queryThegraphIndex(query))['indexingStatuses']
+    if (data === null) {
+      return null
+    }
+    return data as Array<SubgraphIndexingStatus>
+  }
+  let a: Array<SubgraphIndexingStatus> = []
+  const currentVersionQuery = `{ indexingStatusForCurrentVersion(subgraphName:"${subgraphID}")${QUERY_BODY} }`
+  const currentVersionData = (await queryThegraphIndex(currentVersionQuery))[
+    'indexingStatusForCurrentVersion'
+  ]
+  if (currentVersionData === null) {
+    return null
+  }
+  a.push(currentVersionData as SubgraphIndexingStatus)
+  const pendingVersionQuery = `{ indexingStatusForPendingVersion(subgraphName:"${subgraphID}")${QUERY_BODY} }`
+  const pendingVersionData = (await queryThegraphIndex(pendingVersionQuery))[
+    'indexingStatusForPendingVersion'
+  ]
+  if (pendingVersionData !== null) {
+    a.splice(0, 0, pendingVersionData as SubgraphIndexingStatus)
+  }
+  return a
+}
+
+async function queryThegraphIndex(query: string): Promise<any> {
+  return (
+    await axios.post(
+      'https://api.thegraph.com/index-node/graphql',
+      JSON.stringify({ query })
+    )
+  ).data.data
+}
